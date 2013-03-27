7e64245ab610dd03e2b2e94950c67934570a0065
jmribao <jmribao@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2013-03-27 08:02:04 +0000

    Resource external

--- trunk/docroot/WEB-INF/src/com/liferay/lms/ResourceExternalActivity.java
+++ trunk/docroot/WEB-INF/src/com/liferay/lms/ResourceExternalActivity.java
@@ -55,8 +55,7 @@ public class ResourceExternalActivity extends MVCPortlet {
 
 	public void selectResource(ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
-	
-		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
 		String jspPage = ParamUtil.getString(actionRequest, "jspPage");
 		long actId = ParamUtil.getLong(actionRequest, "actId", 0);
 		long entryId = ParamUtil.getLong(actionRequest, "entryId", 0);
@@ -78,17 +77,8 @@ public class ResourceExternalActivity extends MVCPortlet {
 	
 	String jspPage = ParamUtil.getString(actionRequest, "jspPage");
 	long actId = ParamUtil.getLong(actionRequest, "actId", 0);
-	long entryId = ParamUtil.getLong(actionRequest, "entryId", 0);
-	ServiceContext serviceContext = ServiceContextFactory.getInstance(LearningActivity.class.getName(), actionRequest);
-	String fileName = request.getFileName("fileName");
-	String fileName2 = request.getFileName("fileName2");
-	boolean deleteVideo=ParamUtil.getBoolean(request, "deletevideo",false);
-	boolean deleteComplementary=ParamUtil.getBoolean(request, "deletecomplementary",false);
 	String description = request.getParameter("description");
 	String youtubecode=ParamUtil.getString(request,"youtubecode","");
-	String title = fileName;// + " uploaded by " + user.getFullName();
-	AssetEntry entry=null;
-	AssetEntry entry2=null;
 	LearningActivity larn = LearningActivityServiceUtil.getLearningActivity(actId);
 	String extraContent=larn.getExtracontent();
 	Document document = SAXReaderUtil.createDocument();
@@ -98,100 +88,8 @@ public class ResourceExternalActivity extends MVCPortlet {
 		document=SAXReaderUtil.read(extraContent);
 		rootElement =document.getRootElement();
 	}
-	if(deleteVideo)
-	{
-		Element video=rootElement.element("video");
-		if(!video.attributeValue("id","").equals(""))
-		{
-			AssetEntry videoAsset= AssetEntryLocalServiceUtil.getAssetEntry(Long.parseLong(video.attributeValue("id")));
-			FileEntry videofile=DLAppLocalServiceUtil.getFileEntry(videoAsset.getClassPK());
-			//TODO DLFileVersion videofileVersion = videofile.getFileVersion();
-			DLAppLocalServiceUtil.deleteFileEntry(videofile.getFileEntryId());
-		}
-		video.detach();
-		rootElement.remove(video);
-		
-		
-	}
-	if(deleteComplementary)
-	{
-		Element documento=rootElement.element("document");
-		if(!documento.attributeValue("id","").equals(""))
-		{
-			AssetEntry videoAsset= AssetEntryLocalServiceUtil.getAssetEntry(Long.parseLong(documento.attributeValue("id")));
-			FileEntry videofile=DLAppLocalServiceUtil.getFileEntry(videoAsset.getClassPK());
-			//TODO DLFileVersion videofileVersion = videofile.getFileVersion();
-			DLAppLocalServiceUtil.deleteFileEntry(videofile.getFileEntryId());
-		}
-		documento.detach();
-		rootElement.remove(documento);
-		
-	}
 	
-	if(fileName!=null && !fileName.equals(""))
-	{
-		File file = request.getFile("fileName");
-		long filesize=	request.getSize("fileName");
-		
-		Element video=rootElement.element("video");
-		if(video!=null &&!video.attributeValue("id","").equals(""))
-		{
-			AssetEntry videoAsset= AssetEntryLocalServiceUtil.getAssetEntry(Long.parseLong(video.attributeValue("id")));
-			DLFileEntry videofile=DLFileEntryLocalServiceUtil.getDLFileEntry(videoAsset.getClassPK());
-			DLFileVersion videofileVersion = videofile.getFileVersion();
-			DLFileEntryLocalServiceUtil.deleteFileEntry(videofile.getFileEntryId());
-		}
-		long folderId=createDLFolders(actionRequest, themeDisplay.getUserId(),themeDisplay.getScopeGroupId(), serviceContext);
-		InputStream istrem=new FileInputStream(file);
-		String mimeType=MimeTypesUtil.getContentType(file.getName());
-		DLFileEntry dlDocument = DLFileEntryLocalServiceUtil.addFileEntry(
-				themeDisplay.getUserId(), themeDisplay.getScopeGroupId(), themeDisplay.getScopeGroupId(), folderId, fileName,mimeType,
-				title, title,"",0,new HashMap<String,Fields>(), file,
-				istrem,filesize, serviceContext);
-			
-			//Damos permisos al archivo para usuarios de comunidad.
-		DLFileEntryLocalServiceUtil.addFileEntryResources(dlDocument, true, false);
-		entry=AssetEntryLocalServiceUtil.getEntry(DLFileEntry.class.getName(), dlDocument.getPrimaryKey())	;
-		}
-
-	if(fileName2!=null && !fileName2.equals(""))
-	{
-		File file = request.getFile("fileName2");
-		long filesize=	request.getSize("fileName2");
-		Element documento=rootElement.element("document");
-		if(documento!=null&&!documento.attributeValue("id","").equals(""))
-		{
-			AssetEntry videoAsset= AssetEntryLocalServiceUtil.getAssetEntry(Long.parseLong(documento.attributeValue("id")));
-			DLFileEntry videofile=DLFileEntryLocalServiceUtil.getDLFileEntry(videoAsset.getClassPK());
-			DLFileVersion videofileVersion = videofile.getFileVersion();
-			DLFileEntryLocalServiceUtil.deleteFileEntry(videofile.getFileEntryId());
-		}
-		long folderId=createDLFolders(actionRequest, themeDisplay.getUserId(),themeDisplay.getScopeGroupId(), serviceContext);
-		InputStream istrem=new FileInputStream(file);
-		String mimeType=MimeTypesUtil.getContentType(file.getName());
-		
-		DLFileEntry dlDocument = DLFileEntryLocalServiceUtil.addFileEntry(
-				themeDisplay.getUserId(), themeDisplay.getScopeGroupId(), themeDisplay.getScopeGroupId(), folderId, fileName,mimeType,
-				title, title,"",0,new HashMap<String,Fields>(), file,
-				istrem,filesize, serviceContext);
-			
-			//Damos permisos al archivo para usuarios de comunidad.
-		DLFileEntryLocalServiceUtil.addFileEntryResources(dlDocument, true, false);
-		entry2=AssetEntryLocalServiceUtil.getEntry(DLFileEntry.class.getName(), dlDocument.getPrimaryKey())	;
-		if(entry2!=null)
-		{
-			if(documento!=null)
-			{
-				documento.detach();
-				rootElement.remove(documento);
-			}
-			documento=SAXReaderUtil.createElement("document");
-			documento.addAttribute("id", Long.toString(entry2.getEntryId()));
-			rootElement.add(documento);
-		}
-	}
-	
-	if(entry!=null || !"".equals(youtubecode))
+	if(!"".equals(youtubecode))
 	{
 		Element video=rootElement.element("video");
 		if(video!=null)
@@ -200,21 +98,8 @@ public class ResourceExternalActivity extends MVCPortlet {
 			rootElement.remove(video);
 		}
 		video = SAXReaderUtil.createElement("video");
-		if(entry!=null)
-		{
-			video.addAttribute("id", Long.toString(entry.getEntryId()));
-			video.setText("");
-		}
-		else
-		{
-			
-			video.setText(youtubecode);
-		}
-		
+		video.setText(youtubecode);		
 		rootElement.add(video);
-		
-		
-		
 	}
 	larn.setExtracontent(document.formattedString());
 	larn.setDescription( description,themeDisplay.getLocale());
@@ -249,38 +134,9 @@ else
 				renderRequest.setAttribute(WebKeys.PORTLET_CONFIGURATOR_VISIBILITY, Boolean.FALSE);
 			}
 		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (SystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}			
 }
 }
-	private long createDLFolders(ActionRequest request,Long userId,Long groupId,ServiceContext serviceContext) throws PortalException, SystemException{
-		//Variables for folder ids
-		Long dlMainFolderId = 0L;
-		//Search for folder in Document Library
-        boolean dlMainFolderFound = false;
-        //Get main folder
-        try {
-        	//Get main folder
-        	Folder dlFolderMain = DLAppLocalServiceUtil.getFolder(groupId,DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,DOCUMENTLIBRARY_MAINFOLDER);
-        	dlMainFolderId = dlFolderMain.getFolderId();
-        	dlMainFolderFound = true;
-        	//Get portlet folder
-        } catch (Exception ex){
-        	ex.printStackTrace();//Not found Main Folder
-        }
-        //Create main folder if not exist
-        if(!dlMainFolderFound){
-        	Folder newDocumentMainFolder = DLAppLocalServiceUtil.addFolder(userId, groupId,DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, DOCUMENTLIBRARY_MAINFOLDER, DOCUMENTLIBRARY_MAINFOLDER, serviceContext);
-        	dlMainFolderId = newDocumentMainFolder.getFolderId();
-        	dlMainFolderFound = true;
-        }
-        //Create portlet folder if not exist
-     
-  
-        return dlMainFolderId;
-	}
+
 }
