10956ba4714be3e1256bd6ab042c560cc72ec4e8
jmribao <jmribao@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2013-03-27 10:52:51 +0000

    Esqueleto de la tarea de evaluación

---
+++ docroot/WEB-INF/src/com/liferay/lms/learningactivity/TaskEvaluationLearningActivityType.java
@@ -0,0 +1,32 @@
+package com.liferay.lms.learningactivity;
+
+import java.util.Locale;
+
+import com.liferay.lms.asset.TaskEvaluationAssetRenderer;
+import com.liferay.lms.model.LearningActivity;
+import com.liferay.portlet.asset.model.AssetRenderer;
+
+public class TaskEvaluationLearningActivityType extends BaseLearningActivityType {
+
+	@Override
+	public AssetRenderer getAssetRenderer(LearningActivity larn) {
+		return new TaskEvaluationAssetRenderer(larn);
+	}
+	
+	@Override
+	public boolean hasEditDetails() {
+		return false;
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		
+		return "evaluation";
+	}
+
+	@Override
+	public long getTypeId() {
+		return 8;
+	}
+
+}
