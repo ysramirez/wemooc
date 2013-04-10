7da729dd9c114355dcd841dc9b4e9d0cd62163ec
alejandrogp <alejandrogp@gmail.com>
2013-04-10 11:36:01 +0000

    Nuevos portlets de la 6.0 a la 6.1

--- docroot/WEB-INF/src/com/liferay/lms/service/impl/ModuleResultLocalServiceImpl.java
+++ docroot/WEB-INF/src/com/liferay/lms/service/impl/ModuleResultLocalServiceImpl.java
@@ -14,16 +14,35 @@
 
 package com.liferay.lms.service.impl;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import com.liferay.lms.model.Course;
 import com.liferay.lms.model.LearningActivity;
 import com.liferay.lms.model.LearningActivityResult;
 import com.liferay.lms.model.ModuleResult;
+import com.liferay.lms.model.P2pActivity;
+import com.liferay.lms.model.module;
+import com.liferay.lms.service.CourseLocalServiceUtil;
+import com.liferay.lms.service.LearningActivityLocalServiceUtil;
+import com.liferay.lms.service.LearningActivityResultLocalServiceUtil;
+import com.liferay.lms.service.ModuleResultLocalServiceUtil;
+import com.liferay.lms.service.moduleLocalServiceUtil;
 import com.liferay.lms.service.base.ModuleResultLocalServiceBaseImpl;
 import com.liferay.portal.kernel.dao.orm.Criterion;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserGroupLocalServiceUtil;
+import com.liferay.portal.service.UserGroupServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.service.persistence.GroupUtil;
 
 /**
  * The implementation of the module result local service.
@@ -53,6 +72,31 @@ public class ModuleResultLocalServiceImpl extends ModuleResultLocalServiceBaseIm
 		return moduleResult;	
 	}
 
+	/**
+	 * No debería haber nunca más de un result para el mismo usuario y modulo.
+	 * Se hace para eliminar los duplicados.
+	 * @param moduleId
+	 * @param userId
+	 * @return La lista de todos los moduleresult del usuario
+	 * @throws SystemException
+	 */
+	public List<ModuleResult> getListModuleResultByModuleAndUser(long moduleId, long userId) throws SystemException {
+		
+		List<ModuleResult> res = new ArrayList<ModuleResult>();
+		
+		DynamicQuery consulta = DynamicQueryFactoryUtil.forClass(ModuleResult.class)
+				.add(PropertyFactoryUtil.forName("moduleId").eq(moduleId))
+				.add(PropertyFactoryUtil.forName("userId").eq(userId));
+	
+		List<ModuleResult> results = moduleResultPersistence.findWithDynamicQuery(consulta);
+
+		if(results!=null && !results.isEmpty()){
+			res = results;
+		}
+				
+		return res;
+	}
+	
 	public long countByModule(long moduleId)
 		throws SystemException {
 
@@ -123,4 +167,147 @@ public class ModuleResultLocalServiceImpl extends ModuleResultLocalServiceBaseIm
 			courseResultLocalService.update(moduleResult);
 		}
 	}
+	
+	public int updateAllUsers(long groupId, long moduleId) throws PortalException, SystemException {
+		
+		//Obtenemos la lista de users del curso.
+		//List<User> usersList = UserLocalServiceUtil.getUserGroupUsers(groupId);
+		List<User> usersList = GroupUtil.getUsers(groupId);
+				
+		int changes = 0;
+		
+		System.out.println("groupId: "+groupId+", moduleId: "+moduleId+", alumnos en el curso: "+usersList.size());
+		
+		System.out.println("........ START ............");
+		for(User user : usersList){
+			if(update(moduleId, user.getUserId())){
+				changes++;
+			}
+		}
+		System.out.println("Cambiaron "+ changes +" de "+usersList.size()+" alumnos.");
+		System.out.println("........ END ............");
+		
+		return changes;
+	}
+	
+	public void updateAllCoursesAllModulesAllUsers() throws PortalException, SystemException {
+		
+		int changes = 0;
+		
+		List<ModuleResult> moduleResultList = ModuleResultLocalServiceUtil.getModuleResults(0, ModuleResultLocalServiceUtil.getModuleResultsCount());
+		
+		Calendar start = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		System.out.println("........ START ............");
+		
+		int restantes = moduleResultList.size();
+		
+		for(ModuleResult mo : moduleResultList){
+						
+			System.out.println("   :: "+mo.getModuleId()+" :: Restantes: "+ (restantes--));
+			
+			if(update(mo.getModuleId(), mo.getUserId())){
+				changes++;
+			}
+			
+			System.out.println("-----------------------------------------------------------------------");
+			
+		}
+		System.out.println("  Cambiaron "+ changes +" alumnos.");
+		
+		end = Calendar.getInstance();
+		System.out.println(" ## Time start ## "+start.getTime());
+		System.out.println(" ## Time end   ## "+end.getTime());
+		System.out.println("........ END ............");
+	}
+	
+	public boolean update(long moduleId, long userId) throws PortalException, SystemException {
+	
+		boolean passedModule = true;
+		long totalActivities = 0;
+		long activitiesPassed = 0;
+		
+		ModuleResult moduleResult = null;
+		
+		//Actualizar el resultado del modulo en bd.
+		
+		//Obtenemos el moduleResult que tiene el usuario.Si no lo tiene, no lo creamos.
+		if (moduleResultPersistence.countBymu(userId, moduleId) > 0) {
+			
+			moduleResult = moduleResultPersistence.findBymu(userId, moduleId).get(0);
+			
+			List<LearningActivity> learnActList = LearningActivityLocalServiceUtil.getLearningActivitiesOfModule(moduleId);
+
+			for(LearningActivity activity : learnActList){
+				
+				//Si la actividad no es opcional.
+				if(activity.getWeightinmodule() != 0){
+					
+					totalActivities++;
+					
+					LearningActivityResult result = LearningActivityResultLocalServiceUtil.getByActIdAndUserId(activity.getActId(), userId);
+
+					if(result != null && result.isPassed()){
+						 
+						activitiesPassed++;
+						 
+					} else {
+						
+						passedModule = false;
+						
+					}
+					
+				}
+				
+			}
+			
+			if(learnActList.size() == 0){
+				passedModule = false;
+			}
+
+			//Indicamos la media y el resultado del módulo.
+			long result = 0;
+			if(totalActivities > 0){
+				
+				result = activitiesPassed * 100 / totalActivities;
+			}
+			
+			//Sólo actualizamos si cambia el resultado.
+			if(moduleResult.getResult() < result){
+				
+				//Traza
+				User user = UserLocalServiceUtil.getUser(userId);
+				//System.out.println("    *** USER: "+ user.getFullName() +" ("+ userId +")  ***\n           resultOLD: "+moduleResult.getResult()+", passedOLD: "+moduleResult.getPassed()+"\n           resultNEW: "+result+", passedNEW: "+passedModule);
+				module m = moduleLocalServiceUtil.getmodule(moduleId);
+				Course c = CourseLocalServiceUtil.getCourseByGroupCreatedId(m.getGroupId());
+				
+				String text = (moduleResult.getResult()<result)?"Sube":"Baja";
+				try{
+					System.out.println(c.getTitle(Locale.getDefault()) +" ("+ c.getCourseId() +")|"+m.getTitle(Locale.getDefault())+" ("+m.getModuleId()+")|"+moduleResult.getMrId()+"|"+user.getFullName() +" ("+ userId +")|"+moduleResult.getPassed()+"|"+passedModule+"|"+moduleResult.getResult() +"|"+result+"|"+text);
+				}catch (Exception e){System.out.println("ERROR: moduleID: "+moduleId+", userID: "+userId);}
+				
+				moduleResult.setResult(result);
+				moduleResult.setPassed(passedModule);
+
+				//Update en la bd.
+				moduleResultPersistence.update(moduleResult, true);
+				
+				//Actualizar el resultado del curso.
+				courseResultLocalService.update(moduleResult);
+				
+				return true;
+			}else if(moduleResult.getResult() > result){
+				User user = UserLocalServiceUtil.getUser(userId);
+				module m = moduleLocalServiceUtil.getmodule(moduleId);
+				Course c = CourseLocalServiceUtil.getCourseByGroupCreatedId(m.getGroupId());
+				
+				System.out.println(c.getTitle(Locale.getDefault()) +" ("+ c.getCourseId() +")|"+m.getTitle(Locale.getDefault())+" ("+m.getModuleId()+")|"+moduleResult.getMrId()+"|"+user.getFullName() +" ("+ userId +")|"+moduleResult.getPassed()+"|"+passedModule+"|"+moduleResult.getResult() +"|"+result+"|Baja");
+			}
+		}
+
+		return false;
+
+			
+	}
+	
 }
