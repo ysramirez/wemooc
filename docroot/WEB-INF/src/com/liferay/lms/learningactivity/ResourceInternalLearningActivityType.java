1f3d80bb4303fbaf0583b02de3d64ad967e704e4
jmribao <jmribao@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2013-04-29 11:59:26 +0000

    guardado id

--- docroot/WEB-INF/src/com/liferay/lms/learningactivity/ResourceInternalLearningActivityType.java
+++ docroot/WEB-INF/src/com/liferay/lms/learningactivity/ResourceInternalLearningActivityType.java
@@ -1,7 +1,19 @@
 package com.liferay.lms.learningactivity;
 
+import java.io.IOException;
+
+import javax.portlet.PortletResponse;
+
 import com.liferay.lms.asset.ResourceInternalAssetRenderer;
 import com.liferay.lms.model.LearningActivity;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.upload.UploadRequest;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.DocumentException;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portlet.asset.model.AssetRenderer;
 
 public class ResourceInternalLearningActivityType extends BaseLearningActivityType 
@@ -37,6 +49,46 @@ public class ResourceInternalLearningActivityType extends BaseLearningActivityTy
 	public long getTypeId() {
 		return 7;
 	}
+	
+	@Override
+	public String getExpecificContentPage() {
+		return "/html/resourceInternalActivity/admin/edit.jsp";
+	}
+	
+	@Override
+	public boolean hasEditDetails() {
+		return false;
+	}
 
+	
+	@Override
+	public void setExtraContent(UploadRequest uploadRequest,
+			PortletResponse portletResponse, LearningActivity learningActivity)
+			throws PortalException, SystemException, DocumentException,IOException {
+		
+		Document document = null;
+		Element rootElement = null;
+		if((learningActivity.getExtracontent()==null)||(learningActivity.getExtracontent().trim().length()==0)){
+			document = SAXReaderUtil.createDocument();
+			rootElement = document.addElement("resourceInternal");
+		}
+		else
+		{
+			document=SAXReaderUtil.read(learningActivity.getExtracontent());
+			rootElement =document.getRootElement();
+		}
+
+		Element assetEntry=rootElement.element("assetEntry");
+		if(assetEntry!=null)
+		{
+			assetEntry.detach();
+			rootElement.remove(assetEntry);
+		}
+		assetEntry = SAXReaderUtil.createElement("assetEntry");
+		assetEntry.setText(ParamUtil.getString(uploadRequest,"assetEntryId","0"));		
+		rootElement.add(assetEntry);	
+		
+		learningActivity.setExtracontent(document.formattedString());	
+	}
 	
 }
