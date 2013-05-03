eb686d1e6342570890cdbc6813a73c0b690abf1a
martinrobertog <martinrobertog@gmail.com>
2013-05-03 06:30:37 +0000

    Cambio del Mensaje de 2013 TELEFONICA LEARNING SERVICES.

--- docroot/WEB-INF/src/com/liferay/lms/service/base/P2pActivityServiceBaseImpl.java
+++ docroot/WEB-INF/src/com/liferay/lms/service/base/P2pActivityServiceBaseImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * 2013 TELEFONICA LEARNING SERVICES. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
