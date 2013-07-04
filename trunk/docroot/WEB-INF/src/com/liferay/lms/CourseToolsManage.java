abba85af1698a34d485004f3da1d6444b8feeb29
jmribao <jmribao@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2013-07-04 10:52:45 +0000

    #10820

--- trunk/docroot/WEB-INF/src/com/liferay/lms/CourseToolsManage.java
+++ trunk/docroot/WEB-INF/src/com/liferay/lms/CourseToolsManage.java
@@ -6,7 +6,12 @@ import javax.portlet.ActionResponse;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
@@ -22,6 +27,17 @@ public class CourseToolsManage extends MVCPortlet
 			}
 			String layoutid=ParamUtil.getString(request, "layoutid");
 			Layout ellayout=LayoutLocalServiceUtil.getLayout(Long.parseLong(layoutid));
+			if(ellayout.getHidden()) {
+				ellayout.setHidden(true);
+				ResourcePermissionServiceUtil.removeResourcePermission(ellayout.getGroupId(), themeDisplay.getCompanyId(), Layout.class.getName(), 
+						ResourceConstants.SCOPE_INDIVIDUAL, layoutid, RoleLocalServiceUtil.getRole(themeDisplay.getCompanyId(), RoleConstants.SITE_MEMBER).getRoleId(), ActionKeys.VIEW);
+			}
+			else {
+				ellayout.setHidden(false);
+				ResourcePermissionServiceUtil.addResourcePermission(ellayout.getGroupId(), themeDisplay.getCompanyId(), Layout.class.getName(), 
+						ResourceConstants.SCOPE_INDIVIDUAL, layoutid, RoleLocalServiceUtil.getRole(themeDisplay.getCompanyId(), RoleConstants.SITE_MEMBER).getRoleId(), ActionKeys.VIEW);	
+			}
+			
 			ellayout.setHidden(!ellayout.getHidden());
 			LayoutLocalServiceUtil.updateLayout(ellayout);
 			//response.setRenderParameters(request.getParameterMap());			
