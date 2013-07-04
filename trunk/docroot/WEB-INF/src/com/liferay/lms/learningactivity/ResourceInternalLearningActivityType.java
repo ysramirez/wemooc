7405b0567053533f1f21de66096383964ee64cde
virginiatlsgc@gmail.com <virginiatlsgc@gmail.com@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2013-07-04 08:46:22 +0000

    BUG #10138 estudiante > una clase > recurso Intenro ( recurso mediateca)

--- trunk/docroot/WEB-INF/src/com/liferay/lms/learningactivity/ResourceInternalLearningActivityType.java
+++ trunk/docroot/WEB-INF/src/com/liferay/lms/learningactivity/ResourceInternalLearningActivityType.java
@@ -1,23 +1,61 @@
 package com.liferay.lms.learningactivity;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
 
+import javax.persistence.AssociationOverrides;
+import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 
+import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.lms.asset.ResourceInternalAssetRenderer;
 import com.liferay.lms.model.LearningActivity;
 import com.liferay.lms.service.ClpSerializer;
+import com.liferay.lms.service.LearningActivityLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.upload.UploadRequest;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.DocumentException;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.model.PortletConstants;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.model.AssetEntry;
 import com.liferay.portlet.asset.model.AssetRenderer;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileVersion;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.persistence.DLFileVersionUtil;
+import com.liferay.portlet.documentlibrary.util.ImageProcessorUtil;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import com.tls.lms.util.LiferaylmsUtil;
 
 public class ResourceInternalLearningActivityType extends BaseLearningActivityType 
 {
@@ -73,6 +111,54 @@ public class ResourceInternalLearningActivityType extends BaseLearningActivityTy
 			PortletResponse portletResponse, LearningActivity learningActivity)
 			throws PortalException, SystemException, DocumentException,IOException {
 		
+		String assetId = ParamUtil.getString(uploadRequest,"assetEntryId","0");
+		
+		
+		try{
+			PortletRequest actionRequest = (PortletRequest)uploadRequest.getAttribute(JavaConstants.JAVAX_PORTLET_REQUEST);
+			ThemeDisplay themeDisplay = (ThemeDisplay) uploadRequest.getAttribute(WebKeys.THEME_DISPLAY);
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(LearningActivity.class.getName(), actionRequest);
+
+			//Obtengo el assetEntry y compruebo si es un recurso de tipo Documento y Multimedia comprobando
+			//si existe un elemento DLFileEntry con id=assetEntry.getClassPK
+			AssetEntry docAsset= AssetEntryLocalServiceUtil.getAssetEntry(Long.valueOf(assetId));
+			FileEntry docfile=DLAppLocalServiceUtil.getFileEntry(docAsset.getClassPK());
+			if(docfile != null){
+				//Creo un directorio propio de la actividad o lo recupero si ya existe.
+				Folder folder = createFoldersForLearningActivity(themeDisplay.getUserId(), themeDisplay.getScopeGroupId(), learningActivity.getActId(), learningActivity.getTitle(Locale.getDefault()), serviceContext);
+				FileEntry newFile = null;
+				//Recupero el archivo asociado a la actividad si ya existia y lo limpio para crear el nuevo
+				java.util.List<FileEntry> files = DLAppLocalServiceUtil.getFileEntries(themeDisplay.getScopeGroupId(), folder.getFolderId());
+				if(files != null && files.size()>0){
+					int i=0;
+					for(FileEntry file:files){
+						if(i==0)
+							newFile = DLAppLocalServiceUtil.updateFileEntry(themeDisplay.getUserId(), file.getFileEntryId(), Long.toString(learningActivity.getActId()), docfile.getMimeType(), docfile.getTitle(), docfile.getDescription(), "", false, docfile.getContentStream(),docfile.getSize(), serviceContext);
+						else DLAppLocalServiceUtil.deleteFileEntry(file.getFileEntryId());
+					}
+				}else
+					newFile = DLAppLocalServiceUtil.addFileEntry(themeDisplay.getUserId(), themeDisplay.getScopeGroupId(), folder.getFolderId(), Long.toString(learningActivity.getActId()), docfile.getMimeType(), docfile.getTitle(), docfile.getDescription(), "", docfile.getContentStream(), docfile.getSize(), serviceContext);
+				//Recupero el asset asociado al nuevo archivo local del curso.
+				AssetEntry newAsset = AssetEntryLocalServiceUtil.getEntry(docAsset.getClassName(), newFile.getFileEntryId());
+				//Actualizo el extraContentTmp para que apunte al recurso local y no al global
+				assetId = Long.toString(newAsset.getEntryId());
+
+				//Actualizo los permisos del recurso.
+				Role siteMemberRole = RoleLocalServiceUtil.getRole(themeDisplay.getCompanyId(), RoleConstants.SITE_MEMBER);
+				String[] actIds = {ActionKeys.VIEW};
+				try {
+					LiferaylmsUtil.setPermission(themeDisplay, DLFolder.class.getName(), siteMemberRole, actIds, folder.getFolderId());
+					LiferaylmsUtil.setPermission(themeDisplay, DLFileEntry.class.getName(), siteMemberRole, actIds, newFile.getFileEntryId());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				ImageProcessorUtil.generateImages(newFile.getLatestFileVersion());
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		Document document = null;
 		Element rootElement = null;
 		if((learningActivity.getExtracontent()==null)||(learningActivity.getExtracontent().trim().length()==0)){
@@ -92,11 +178,26 @@ public class ResourceInternalLearningActivityType extends BaseLearningActivityTy
 			rootElement.remove(assetEntry);
 		}
 		assetEntry = SAXReaderUtil.createElement("assetEntry");
-		assetEntry.setText(ParamUtil.getString(uploadRequest,"assetEntryId","0"));		
+		assetEntry.setText(assetId);		
 		rootElement.add(assetEntry);	
 		
 		learningActivity.setExtracontent(document.formattedString());	
 	}
+		
+
+
+		/**
+		 * Primero se busca si ya existe, si existe se devuelve y sino se crea uno nuevo.
+		 */
+		private Folder createFoldersForLearningActivity(Long userId, Long groupId, Long actId, String title, ServiceContext serviceContext) throws PortalException, SystemException{
+			Folder newFolder = null;
+			try {
+				newFolder = DLAppLocalServiceUtil.getFolder(groupId, 0, String.valueOf(actId));
+			} catch (Exception e) {
+				newFolder = DLAppLocalServiceUtil.addFolder(userId, groupId,0, String.valueOf(actId), title, serviceContext);
+			}
+	    	return newFolder;
+		}
 	
 	@Override
 	public String getDescription() {
