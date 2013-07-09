8d022413e3334cd72ee9d8215f3eaffe053394ab
virginiatlsgc@gmail.com <virginiatlsgc@gmail.com@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2013-07-09 12:52:47 +0000

    BUG #11057 Alumno > Calificaciones > Tarea presencial

--- trunk/docroot/WEB-INF/src/com/tls/lms/util/LiferaylmsUtil.java
+++ trunk/docroot/WEB-INF/src/com/tls/lms/util/LiferaylmsUtil.java
@@ -1,15 +1,24 @@
 package com.tls.lms.util;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.liferay.lms.model.LearningActivity;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.Resource;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.ResourceBlockLocalServiceUtil;
 import com.liferay.portal.service.ResourceBlockServiceUtil;
 import com.liferay.portal.service.ResourceLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.service.ResourcePermissionServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 
 public class LiferaylmsUtil {
@@ -30,6 +39,26 @@ public class LiferaylmsUtil {
 					themeDisplay.getScopeGroupId(), themeDisplay.getCompanyId(), resource.getName(),
 					Long.toString(primaryKey), roleIdsToActionIds);	
 		}
+	}
+
+	public static List<LearningActivity> getVisibleActivities(ThemeDisplay themeDisplay,
+			List<LearningActivity> res, PermissionChecker permissionChecker)
+			throws PortalException, SystemException {
+		if(res != null && res.size()>0){
+			Role siteMemberRole = RoleLocalServiceUtil.getRole(themeDisplay.getCompanyId(), RoleConstants.SITE_MEMBER);
+			for(java.util.ListIterator<LearningActivity> itr = res.listIterator(); itr.hasNext();){
+				LearningActivity activity = itr.next();
+				try {
+					if(!ResourcePermissionLocalServiceUtil.hasResourcePermission(activity.getCompanyId(), LearningActivity.class.getName(), 
+							ResourceConstants.SCOPE_INDIVIDUAL,	Long.toString(activity.getActId()),siteMemberRole.getRoleId(), ActionKeys.VIEW)
+							&& !permissionChecker.hasPermission(activity.getGroupId(), LearningActivity.class.getName(), activity.getActId() , "CORRECT"))
+						itr.remove();
+				} catch (SystemException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return res;
 	}
 	
 }
