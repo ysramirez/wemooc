5c890cdcbbadea42931a4921622f9dbd176f9589
virginiatlsgc <virginiatlsgc@gmail.com>
2013-10-17 16:11:12 +0000

    TAREA #10708 Sistema de calificaciones por países. Estructura preparada y funcionando.

--- docroot/WEB-INF/src/com/liferay/lms/learningactivity/calificationtype/CeroToTenCalificationType.java
+++ docroot/WEB-INF/src/com/liferay/lms/learningactivity/calificationtype/CeroToTenCalificationType.java
@@ -1,5 +1,6 @@
 package com.liferay.lms.learningactivity.calificationtype;
 
+import java.text.DecimalFormat;
 import java.util.Locale;
 
 public class CeroToTenCalificationType extends BaseCalificationType{
@@ -31,7 +32,8 @@ public class CeroToTenCalificationType extends BaseCalificationType{
 
 	@Override
 	public String translate(double result) {
-		return Double.toString(result/10);
+		DecimalFormat df = new DecimalFormat("##.#");
+		return df.format(result/10);
 	}
 
 }
