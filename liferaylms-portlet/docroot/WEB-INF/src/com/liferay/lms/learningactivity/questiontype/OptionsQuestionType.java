c251f32eb73b67e81abf4e70d23735dcf4966bec
virginiatlsgc <virginiatlsgc@gmail.com>
2013-12-03 16:54:57 +0000

    INCIDENCIA #19288 Error en el marcado de las respuestas correctas en los test

--- liferaylms-portlet/docroot/WEB-INF/src/com/liferay/lms/learningactivity/questiontype/OptionsQuestionType.java
+++ liferaylms-portlet/docroot/WEB-INF/src/com/liferay/lms/learningactivity/questiontype/OptionsQuestionType.java
@@ -74,8 +74,12 @@ public class OptionsQuestionType extends BaseQuestionType {
 			}else if(arrayAnswersId.contains(answer.getAnswerId())) incorrectAnswered++;
 		}
 
-		if(correctAnswers==correctAnswered && incorrectAnswered==0)	return true;
+		if(isQuestionCorrect(correctAnswers, correctAnswered, incorrectAnswered))return true;
 		else return false;
+	}
+	
+	protected boolean isQuestionCorrect(int correctAnswers, int correctAnswered, int incorrectAnswered){
+		return correctAnswered>0 && incorrectAnswered==0;
 	}
 
 	protected boolean isCorrect(TestAnswer testAnswer){
@@ -146,7 +150,7 @@ public class OptionsQuestionType extends BaseQuestionType {
 			}
 
 			if(feedback){
-				if(correctAnswers==correctAnswered && incorrectAnswered==0)	cssclass=" correct";
+				if(isQuestionCorrect(correctAnswers, correctAnswered, incorrectAnswered))	cssclass=" correct";
 				else cssclass=" incorrect";
 				
 				answersFeedBack = "<div class=\"content_answer\">" + answersFeedBack + "</div>";
