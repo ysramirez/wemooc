6fb681f04179927d8591f2332796ed7d01922b28
alejandrogp <alejandrogp@gmail.com>
2014-01-09 10:11:23 +0000

    Incidencia #20061 - docente>mis clases>actividad test>chrome
    Mejorar la estructura del código que bloquea los campos no editables del extracontent. Se comprueba con UPDATE_ACTIVE.

--- liferaylms-portlet/docroot/WEB-INF/src/com/liferay/lms/learningactivity/TestLearningActivityType.java
+++ liferaylms-portlet/docroot/WEB-INF/src/com/liferay/lms/learningactivity/TestLearningActivityType.java
@@ -131,7 +131,8 @@ public class TestLearningActivityType extends BaseLearningActivityType
 		Course course=CourseLocalServiceUtil.fetchByGroupCreatedId(themeDisplay.getScopeGroupId());
 		Module module = ModuleLocalServiceUtil.getModule(learningActivity.getModuleId());
 				
-	    if((!module.getStartDate().before(new Date()))||(themeDisplay.getPermissionChecker().hasPermission(course.getGroupId(), Course.class.getName(),course.getCourseId(),"COURSEEDITOR")))
+	    if((!module.getStartDate().before(new Date()))||(themeDisplay.getPermissionChecker().hasPermission(course.getGroupId(), Course.class.getName(),course.getCourseId(),"COURSEEDITOR"))
+	    		|| themeDisplay.getPermissionChecker().hasPermission(learningActivity.getGroupId(), LearningActivity.class.getName(),learningActivity.getActId(),"UPDATE_ACTIVE"))
 	    {
 			Document document = null;
 			Element rootElement = null;
