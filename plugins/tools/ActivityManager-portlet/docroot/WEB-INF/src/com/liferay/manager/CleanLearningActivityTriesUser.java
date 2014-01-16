aabd4017717899f4f106f880cca978bf67ef1580
drenedo <drenedo@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2014-01-16 10:09:26 +0000

    Activity Manager

---
+++ plugins/tools/ActivityManager-portlet/docroot/WEB-INF/src/com/liferay/manager/CleanLearningActivityTriesUser.java
@@ -0,0 +1,59 @@
+package com.liferay.manager;
+
+import java.util.List;
+import java.util.Locale;
+
+import com.liferay.lms.model.LearningActivity;
+import com.liferay.lms.model.LearningActivityResult;
+import com.liferay.lms.model.LearningActivityTry;
+import com.liferay.lms.service.LearningActivityResultLocalServiceUtil;
+import com.liferay.lms.service.LearningActivityTryLocalServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+import com.liferay.portal.model.User;
+
+public class CleanLearningActivityTriesUser extends CleanLearningActivity implements MessageListener{
+	Log log = LogFactoryUtil.getLog(CleanLearningActivityTriesUser.class);
+	private LearningActivity la = null;
+	private User user = null;
+	
+	public CleanLearningActivityTriesUser(){
+	}
+
+	public void process() throws Exception{
+		List<LearningActivityTry> lats = LearningActivityTryLocalServiceUtil.getLearningActivityTryByActUser(la.getActId(), user.getUserId());
+		for(LearningActivityTry lat : lats){
+			LearningActivityTryLocalServiceUtil.deleteLearningActivityTry(lat.getLatId());
+		}
+		
+		LearningActivityResult lar = LearningActivityResultLocalServiceUtil.getByActIdAndUserId(la.getActId(), user.getUserId());
+		if(lar!=null){
+			LearningActivityResultLocalServiceUtil.deleteLearningActivityResult(lar);
+		}
+	}
+
+	@Override
+	public void receive(Message message) throws MessageListenerException {
+		setRunning(true);
+		
+		try{
+			this.la = (LearningActivity)message.get("learningActivity");
+			this.user = (User)message.get("user");
+			
+			if(log.isDebugEnabled())log.debug(" LearningActivity: " + la.getTitle(Locale.getDefault()) + " - " + la.getActId() + " - " +user.getFullName());
+			
+			process();
+		}catch(Exception e){
+			if(log.isInfoEnabled())log.info(e.getMessage());
+			if(log.isDebugEnabled())e.printStackTrace();
+		} finally {
+			setRunning(false);
+		}
+		
+		
+	}
+
+}
