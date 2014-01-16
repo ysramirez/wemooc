4d8b0db311e688342613bc10129701d017ff4c78
alejandrogp@gmail.com <alejandrogp@gmail.com@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2014-01-16 10:06:04 +0000

    Borrar intento de la simple activity

--- trunk/plugins/tools/ActivityManager-portlet/docroot/WEB-INF/src/com/liferay/manager/CleanLearningActivityP2P.java
+++ trunk/plugins/tools/ActivityManager-portlet/docroot/WEB-INF/src/com/liferay/manager/CleanLearningActivityP2P.java
@@ -1,5 +1,7 @@
 package com.liferay.manager;
 
+import java.util.List;
+
 import com.liferay.lms.model.LearningActivityResult;
 import com.liferay.lms.model.LearningActivityTry;
 import com.liferay.lms.model.P2pActivity;
@@ -7,6 +9,7 @@ import com.liferay.lms.model.P2pActivityCorrections;
 import com.liferay.lms.service.ClpSerializer;
 import com.liferay.lms.service.LearningActivityResultLocalServiceUtil;
 import com.liferay.lms.service.LearningActivityTryLocalServiceUtil;
+import com.liferay.lms.service.P2pActivityCorrectionsLocalServiceUtil;
 import com.liferay.lms.service.P2pActivityLocalServiceUtil;
 import com.liferay.lms.service.persistence.P2pActivityCorrectionsUtil;
 import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
@@ -36,8 +39,12 @@ public class CleanLearningActivityP2P extends CleanLearningActivity implements M
 			
 			deleteUserUpload(actId, userId);
 			
-		} else if("deleteUserUpload".equals(action)){
+		} else if("deleteUserUploadAndCorrections".equals(action)){
 			
+			//Borrar las asignaciones que tiene asignadas
+			deleteP2PCorrections(actId, userId);
+			
+			//Borrar la propia tarea
 			deleteUserUpload(groupId, moduleId);
 			
 		} 
@@ -72,39 +79,29 @@ public class CleanLearningActivityP2P extends CleanLearningActivity implements M
 		
 	}
 	
-	private void deleteUserAsignations(long actId, long userId){
+	private void deleteP2PCorrections(long actId, long userId){
 		
 		try {
-			
-			P2pActivity p2p = P2pActivityLocalServiceUtil.findByActIdAndUserId(actId, userId);
 			
 			ClassLoader classLoader = (ClassLoader) PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(),"portletClassLoader");
 			DynamicQuery dq=DynamicQueryFactoryUtil.forClass(P2pActivityCorrections.class,classLoader);
 		  	Criterion criterion=PropertyFactoryUtil.forName("actId").eq(actId);
 			dq.add(criterion);
 
-			P2pActivityCorrectionsUtil.findWithDynamicQuery(dq);
+			List<P2pActivityCorrections> corrections = P2pActivityCorrectionsUtil.findWithDynamicQuery(dq);
 			
-			//Las P2P sólo generan un try cuando se hace la entrega, con obtener la última nos es suficiente.
-			LearningActivityTry learnTry = LearningActivityTryLocalServiceUtil.getLastLearningActivityTryByActivityAndUser(actId, userId);
-			LearningActivityResult learnResult = LearningActivityResultLocalServiceUtil.getByActIdAndUserId(actId, userId);
-			
-			//Borrar los registros de la base de datos.
-			LearningActivityResultLocalServiceUtil.deleteLearningActivityResult(learnResult);
-			LearningActivityTryLocalServiceUtil.deleteLearningActivityTry(learnTry);
-			P2pActivityLocalServiceUtil.deleteP2pActivity(p2p);
-			
-		} catch (SystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			for(P2pActivityCorrections correction:corrections){
+				System.out.println(" correction : " + correction.getActId()+" - "+correction.getUserId() );
+				P2pActivityCorrectionsLocalServiceUtil.deleteP2pActivityCorrections(correction);
+				
+			}
+
 		}catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
+
 	
 }
