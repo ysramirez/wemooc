8a7a992f046b6282e8e4f6cfb430122b18977dd4
drenedo <drenedo@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2014-01-20 08:28:05 +0000

    Activity Manager

--- plugins/tools/ActivityManager-portlet/docroot/WEB-INF/sql/tables.sql
+++ plugins/tools/ActivityManager-portlet/docroot/WEB-INF/sql/tables.sql
@@ -27,3 +27,33 @@ create table lms_LearningActivityTryDeleted (
 	tryResultData VARCHAR(75) null,
 	comments VARCHAR(75) null
 );
+
+create table lmssa_ActManAudit (
+	uuid_ VARCHAR(75) null,
+	actManAuditId LONG not null primary key,
+	companyId LONG,
+	groupId LONG,
+	userId LONG,
+	courseId LONG,
+	className VARCHAR(75) null,
+	start_ DATE null,
+	end_ DATE null,
+	state_ VARCHAR(75) null,
+	userTargetId LONG,
+	actId LONG
+);
+
+create table lmssa_LearningActivityTryDeleted (
+	uuid_ VARCHAR(75) null,
+	latDelId LONG not null primary key,
+	actManAuditId LONG,
+	latId LONG,
+	actId LONG,
+	userId LONG,
+	startDate DATE null,
+	result LONG,
+	endDate DATE null,
+	tryData VARCHAR(75) null,
+	tryResultData VARCHAR(75) null,
+	comments VARCHAR(75) null
+);
