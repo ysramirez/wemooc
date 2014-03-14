b109366d772bf43de3202a7e6ba0b99ea13eee74
drenedo <drenedo@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2014-03-14 10:44:55 +0000

    Asset changes

--- trunk/plugins/activities/lti-portlet/docroot/WEB-INF/src/com/liferay/lms/lti/asset/LTIAssetRenderer.java
+++ trunk/plugins/activities/lti-portlet/docroot/WEB-INF/src/com/liferay/lms/lti/asset/LTIAssetRenderer.java
@@ -1,125 +1,33 @@
 package com.liferay.lms.lti.asset;
 
-import java.util.Locale;
-
+import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
+import com.liferay.lms.asset.LearningActivityBaseAssetRenderer;
+import com.liferay.lms.lti.LTILearningActivityType;
 import com.liferay.lms.model.LearningActivity;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
-import com.liferay.portal.security.permission.ActionKeys;
-import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portlet.asset.model.BaseAssetRenderer;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portlet.PortletURLFactoryUtil;
 
-public class LTIAssetRenderer extends BaseAssetRenderer {
-	private static Log log = LogFactoryUtil.getLog(LTIAssetRenderer.class);
+public class LTIAssetRenderer extends LearningActivityBaseAssetRenderer {
 
-	private LearningActivity _learningactivity;
+	public LTIAssetRenderer(LearningActivity learningactivity,
+			LTILearningActivityType connectLearningActivityType)
+			throws SystemException, PortalException {
+		super(learningactivity,connectLearningActivityType,false);
+	}
 	
-	public LTIAssetRenderer (LearningActivity learningactivity) {
-		_learningactivity = learningactivity;
-	}
-
 	@Override
-	public long getClassPK() {
-		return _learningactivity.getActId();
-	}
-
-	@Override
-	public long getGroupId() {
-		return _learningactivity.getGroupId();
-	}
-
-	public String getSummary() {
-		return _learningactivity.getDescription();
-	}
-
-	public String getTitle() {
-		return _learningactivity.getTitle();
-	}
-
-	@Override
-	public long getUserId() {
-		return _learningactivity.getUserId();
-	}
-
-	@Override
-	public String getUuid() {
-		return _learningactivity.getUuid();
-	}
-
-	@Override
-	public String render(RenderRequest request, RenderResponse response, String template) throws Exception {
-		if(log.isDebugEnabled())log.debug("render");
-		if (template.equals(TEMPLATE_FULL_CONTENT)) {
-			request.setAttribute("learningactivity", _learningactivity);
-			return "/html/asset/" + template + ".jsp";
-		}
-
-		return "/html/asset/" + template + ".jsp";
-	}
-
-	@Override
-	public PortletURL getURLEdit(LiferayPortletRequest liferayPortletRequest,
+	protected PortletURL getURLEditDetails(LiferayPortletRequest liferayPortletRequest,
 			LiferayPortletResponse liferayPortletResponse) throws Exception {
-		if(log.isDebugEnabled())log.debug("getURLEdit");
-		PortletURL portletURL = liferayPortletResponse.createActionURL( "lti_WAR_ltiportlet");
-		portletURL.setParameter("javax.portlet.action", "edit");
-		portletURL.setParameter("p_p_id", String.valueOf(_learningactivity.getActId()));
-		
+		PortletURL portletURL = PortletURLFactoryUtil.create(liferayPortletRequest,getPortletId(),getLayout().getPlid(),PortletRequest.RENDER_PHASE);	
 		portletURL.setParameter("mvcPath", "/html/lti/admin/edit.jsp");
-		portletURL.setParameter("jspPage", "/html/lti/admin/edit.jsp"); 
-	    portletURL.setParameter("actId",Long.toString( _learningactivity.getActId()));
-	    
+		portletURL.setParameter("actionEditingDetails", StringPool.TRUE);
+	    portletURL.setParameter("resId",Long.toString( getLearningactivity().getActId()));
 	    return portletURL;
-	}
-	
-	@Override
-	public String getURLViewInContext(
-			LiferayPortletRequest liferayPortletRequest,
-			LiferayPortletResponse liferayPortletResponse,
-			String noSuchEntryRedirect) throws Exception {
-		if(log.isDebugEnabled())log.debug("getURLViewInContext");
-
-		PortletURL portletURL = liferayPortletResponse.createRenderURL("lti_WAR_ltiportlet");
-		portletURL.setParameter("jspPage", "/html/lti/view.jsp");
-		portletURL.setParameter("actId",Long.toString( _learningactivity.getActId()));
-	
-		return portletURL.toString();
-	}
-
-	@Override
-	public String getViewInContextMessage() {
-		if(log.isDebugEnabled())log.debug("getViewInContextMessage");
-		return "View";
-	}
-
-	@Override
-	public boolean hasEditPermission(PermissionChecker permissionChecker) throws PortalException, SystemException {
-		
-		if(permissionChecker.hasPermission(this.getGroupId(), LearningActivity.class.getName(), this.getClassPK(), ActionKeys.UPDATE))
-			return true;
-
-		return false;
-	}
-
-	public String getSummary(Locale arg0){
-		return getSummary();
-	}
-
-	public String getTitle(Locale arg0){
-		return getTitle();
-	}
-	
-	@Override
-	public String getUserName() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
