300ed69b1d360c5c72a8e04e5527acad65c29bad
pereira.lopez.1987 <pereira.lopez.1987@gmail.com>
2015-01-30 08:12:19 +0000

    Arreglo de incidencia que importaba erroneamente las preguntas de las encuentas Incidencia 47881

--- liferaylms-portlet/docroot/WEB-INF/src/com/tls/lms/util/LiferaylmsUtil.java
+++ liferaylms-portlet/docroot/WEB-INF/src/com/tls/lms/util/LiferaylmsUtil.java
@@ -27,6 +27,13 @@ public class LiferaylmsUtil {
 	
 	public static final int defaultStartYear = 2012; //porque es necesario mantener los valores antiguos para los cursos antiguos
 	public static final int defaultEndYear = Calendar.getInstance().get(Calendar.YEAR) + 8;
+    public static final String CHARSET_UTF_8        = "UTF-8".intern();
+    public static final String CHARSET_X_ISO_10646_UCS_4_3412 = "X-ISO-10646-UCS-4-3412".intern(); // Malformed UTF-32
+    public static final String CHARSET_UTF_16BE     = "UTF-16BE".intern();
+    public static final String CHARSET_UTF_16LE     = "UTF-16LE".intern();
+    public static final String CHARSET_UTF_32BE     = "UTF-32BE".intern();
+    public static final String CHARSET_UTF_32LE     = "UTF-32LE".intern();
+    public static final String CHARSET_X_ISO_10646_UCS_4_2143 = "X-ISO-10646-UCS-4-2143".intern();
 
 	public static void setPermission(ThemeDisplay themeDisplay, String classname, Role role, String[] actionIds, long primaryKey) throws Exception{
 		Resource resource =  ResourceLocalServiceUtil.getResource(themeDisplay.getCompanyId(), classname, ResourceConstants.SCOPE_INDIVIDUAL, Long.toString(primaryKey));
@@ -68,4 +75,46 @@ public class LiferaylmsUtil {
 		return res2;
 	}
 	
+	public static String getEncodingTypeOfFile(final byte[] buf, int offset, int length) {
+		
+		String detectedCharset = new String();
+
+		if (length > 3) {
+			int b1 = buf[offset] & 0xFF;
+			int b2 = buf[offset + 1] & 0xFF;
+			int b3 = buf[offset + 2] & 0xFF;
+			int b4 = buf[offset + 3] & 0xFF;
+
+			switch (b1) {
+			case 0xEF:
+				if (b2 == 0xBB && b3 == 0xBF) {
+					detectedCharset = CHARSET_UTF_8;
+				}
+				break;
+			case 0xFE:
+				if (b2 == 0xFF && b3 == 0x00 && b4 == 0x00) {
+					detectedCharset = CHARSET_X_ISO_10646_UCS_4_3412;
+				} else if (b2 == 0xFF) {
+					detectedCharset = CHARSET_UTF_16BE;
+				}
+				break;
+			case 0x00:
+				if (b2 == 0x00 && b3 == 0xFE && b4 == 0xFF) {
+					detectedCharset = CHARSET_UTF_32BE;
+				} else if (b2 == 0x00 && b3 == 0xFF && b4 == 0xFE) {
+					detectedCharset = CHARSET_X_ISO_10646_UCS_4_2143;
+				}
+				break;
+			case 0xFF:
+				if (b2 == 0xFE && b3 == 0x00 && b4 == 0x00) {
+					detectedCharset = CHARSET_UTF_32LE;
+				} else if (b2 == 0xFE) {
+					detectedCharset = CHARSET_UTF_16LE;
+				}
+				break;
+			} // swich end
+		}
+		return detectedCharset;
+	}
+	 
 }
