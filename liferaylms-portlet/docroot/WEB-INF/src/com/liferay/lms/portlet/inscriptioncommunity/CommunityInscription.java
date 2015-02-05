d2e000d48f2098a7a7f78427a94307705f99a59d
drenedo <drenedo@7a7cab44-9a6d-7955-c6e0-f7df98eac81c>
2015-02-05 12:19:51 +0000

    Group created error

--- liferaylms-portlet/docroot/WEB-INF/src/com/liferay/lms/portlet/inscriptioncommunity/CommunityInscription.java
+++ liferaylms-portlet/docroot/WEB-INF/src/com/liferay/lms/portlet/inscriptioncommunity/CommunityInscription.java
@@ -1,15 +1,14 @@
 package com.liferay.lms.portlet.inscriptioncommunity;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.mail.internet.InternetAddress;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.ProcessAction;
-import com.liferay.portal.kernel.exception.SystemException;
 
-import com.liferay.lms.auditing.AuditConstants;
-import com.liferay.lms.auditing.AuditingLogFactory;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.lms.model.Course;
 import com.liferay.lms.service.CourseLocalServiceUtil;
 import com.liferay.mail.service.MailServiceUtil;
@@ -25,6 +24,10 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.MembershipRequestLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -60,7 +63,7 @@ public class CommunityInscription extends MVCPortlet {
 			}
 		}
     	
-    	if(group.getType()!=GroupConstants.TYPE_SITE_RESTRICTED){
+    	if(group.getType()!=GroupConstants.TYPE_SITE_RESTRICTED){ 
     		if(log.isDebugEnabled()){
     			log.debug("Site not restricted!");
     		}
@@ -79,7 +82,60 @@ public class CommunityInscription extends MVCPortlet {
 	    	ServiceContext serviceContext=ServiceContextFactory.getInstance(request);
 	    	MembershipRequestLocalServiceUtil.addMembershipRequest(themeDisplay.getUserId(), themeDisplay.getScopeGroupId(), "Enroll petition", serviceContext);
 	    	SocialActivityLocalServiceUtil.addActivity(themeDisplay.getUserId(), course.getGroupId(), Course.class.getName(), course.getCourseId(), com.liferay.portlet.social.model.SocialActivityConstants.TYPE_SUBSCRIBE, "", course.getUserId());
-	    	
+
+			
+			List<User> allUsers = UserLocalServiceUtil.getGroupUsers(course.getGroupCreatedId());
+			
+			for(User userTmp : allUsers){
+				PermissionChecker permissionChecker;
+				try {
+					permissionChecker = PermissionCheckerFactoryUtil.create(userTmp);
+					if(log.isDebugEnabled())log.debug(userTmp.getFullName());
+					if(permissionChecker.hasPermission(course.getGroupId(),Course.class.getName(),course.getCourseId(),ActionKeys.ASSIGN_MEMBERS)){
+						if(log.isDebugEnabled())log.debug(userTmp.getFullName());
+
+				    	String fromName = PrefsPropsUtil.getString(themeDisplay.getCompanyId(),
+								PropsKeys.ADMIN_EMAIL_FROM_NAME);
+						String fromAddress = PrefsPropsUtil.getString(themeDisplay.getCompanyId(),
+								PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
+						
+						InternetAddress from = new InternetAddress(fromAddress, fromName);
+
+				    	String url = themeDisplay.getURLPortal();
+				    	String urlcourse = themeDisplay.getURLPortal()+"/web"+course.getFriendlyURL(); 
+
+				    	String emailTo = userTmp.getEmailAddress();
+				    	String nameTo = userTmp.getFullName();
+				    	InternetAddress to = new InternetAddress(emailTo, nameTo);
+						
+						String subject = LanguageUtil.format(userTmp.getLocale(),"reply-membership-request-for-x", new String[]{course.getTitle(userTmp.getLocale())});
+				    	String body = StringUtil.replace(
+				    			LanguageUtil.get(userTmp.getLocale(), "reply-membership-body"),
+				    			new String[] {"[$FROM_ADDRESS$]", "[$FROM_NAME$]", "[$PAGE_URL$]","[$PORTAL_URL$]","[$TO_ADDRESS$]","[$TO_NAME$]","[$COURSE_NAME$]"},
+				    			new String[] {fromAddress, fromName, urlcourse, url, emailTo, nameTo,course.getTitle(userTmp.getLocale())});
+				    	
+						try{
+							if(log.isDebugEnabled()){
+								log.debug(from);
+								log.debug(to);
+								log.debug(subject);
+								log.debug(body);
+							}
+							MailMessage mailm = new MailMessage(from, to, subject, body, true);
+							MailServiceUtil.sendEmail(mailm);
+						}
+						catch(Exception ex)
+						{
+							ex.printStackTrace();
+						}
+						
+					}
+				} catch (Exception e) {
+					if(log.isDebugEnabled())e.printStackTrace();
+					if(log.isErrorEnabled())log.error(e.getMessage());
+				}
+			}
+			
 		} catch (PortalException e) {
 			if(log.isDebugEnabled()){
 				e.printStackTrace();
